0715f93ad550de780954a49c527685b7c53464e9
kev-Fuentes <kfuentes@condorlabs.io>
2022-01-09 19:15:21 -0500

    modify index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log('hola mundo ');
 console.log('nuevo')
 console.log('Hola kevin F')
 
-console.log('version 2 agregada')
+console.log('version 2')
 
-console.log('version 3 agregada')
+console.log('version 3')
+
